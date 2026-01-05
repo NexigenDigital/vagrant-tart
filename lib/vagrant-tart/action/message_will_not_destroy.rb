790a4a2e505c5f5488ce1cfe3bb63a9886f369c5
Sn0wCrack <442287+Sn0wCrack@users.noreply.github.com>
2026-01-05 12:59:53 +1100

    fix: bring inline with upstream change

--- lib/vagrant-tart/action/message_will_not_destroy.rb
+++ lib/vagrant-tart/action/message_will_not_destroy.rb
@@ -1,17 +1,21 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: BUSL-1.1
+# forzen_string_literal: true
+
+require "i18n"
 
 module VagrantPlugins
   module Tart
     module Action
+      # Action block to inform the user that the machine will not be destroyed.
       class MessageWillNotDestroy
-        def initialize(app, env)
+        def initialize(app, _env)
           @app = app
         end
 
         def call(env)
           env[:ui].info I18n.t("vagrant.commands.destroy.will_not_destroy",
-                              name: env[:machine].name)
+                               name: env[:machine].name)
           @app.call(env)
         end
       end
